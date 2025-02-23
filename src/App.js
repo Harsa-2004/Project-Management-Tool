1d8929045fb129968ff2788807b000439841368a
Harsa-2004 <147088869+Harsa-2004@users.noreply.github.com>
2025-02-23 17:45:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,39 +58,3 @@ export default App;
 
 
 
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Signup from './signup/Signup';
-// import Login from './Login/Login';
-// import Dashboard from './dashboard/Dashboard';
-// import AdminPage from './components/AdminPage';
-// import ManagerPage from './components/ManagerPage';
-// import TeamPage from './components/TeamPage';
-// import Unauthorized from './components/Unauthorized';
-// import PrivateRoute from './components/PrivateRoute';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import LandingPage from './dashboard/LandingPage';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Public Routes */}
-//         <Route path="/" element={<Signup />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/LandingPage" element={<LandingPage />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-
-//         {/* Role-Based Routes */}
-//         <Route path="/admin" element={<PrivateRoute element={<AdminPage />} allowedRoles={["admin"]} />} />
-//         <Route path="/manager" element={<PrivateRoute element={<ManagerPage />} allowedRoles={["admin", "manager"]} />} />
-//         <Route path="/team" element={<PrivateRoute element={<TeamPage />} allowedRoles={["admin", "manager", "team"]} />} />
-
-//         {/* Unauthorized Page */}
-//         <Route path="/unauthorized" element={<Unauthorized />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
